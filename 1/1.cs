523d5d5a62c073867ac2daed8d942a3ef7dde86c
Bruno Santos <brunosantosprotec@gmail.com>
2019-05-13 10:34:52 -0300

    activities concluded

--- 1/1.cs
+++ 1/1.cs
@@ -1,13 +1,15 @@
 using System;
 
-namespace workspace{
-    class program {
-        static void main(String[] args){
+namespace workspace
+{
+    class program
+    {
+        static void main(String[] args)
+        {
             int[] numeros = new int[50];
 
-            for(int i = 0; i < 50; i++)
+            for (int i = 0; i < 50; i++)
                 numeros[i] = i + 1;
         }
     }
 }
-
